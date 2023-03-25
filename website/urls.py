39c14a33c2db5488ec70699e0ad987d56377a835
iamalinaji <anm33918@gmail.com>
2023-03-25 15:47:01 +0330

    feat: Add static and media URLs to project.

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from website.views import home_view,about_view,contact_view
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('',home_view),
     path('about',about_view),
     path('contact',contact_view)
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
