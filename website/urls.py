3681f37b04d798d596601c8b0ec35b12196b265d
iamalinaji <anm33918@gmail.com>
2023-03-28 01:34:56 +0330

    fix: Correct HTML rendering for all pages.

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from website.views import home_view,about_view,contact_view
-from django.conf import settings
-from django.conf.urls.static import static
+
+app_name='website'
 urlpatterns = [
-    path('',home_view),
-    path('about',about_view),
-    path('contact',contact_view)
+    path('',home_view,name='home'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact')
 ]
