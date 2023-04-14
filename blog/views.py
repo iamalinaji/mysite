2b302499d063166b99301cc60b5d81e5d5f100fb
iamalinaji <anm33918@gmail.com>
2023-04-14 22:02:26 +0330

    Updated method of getting post object in blog single view. Also, applied small improvements in template.

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from blog.models import Post
 from django.utils import timezone
+from django.http import Http404
 import datetime
 
 
 def blog_view(request):
     current_time = timezone.now()
-    posts = Post.objects.filter(published_date__gt=current_time)
+    posts = Post.objects.filter(published_date__lt=current_time)
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
 def blog_single(request,post_id=1):
-    post = Post.objects.get(id=post_id)
+    post = get_object_or_404(Post, pk=post_id)
     context={'post':post}
     post.counted_views+=1
     post.save()
     return render(request,'blog/blog-single.html',context)
-
-
 
 def test(request):
     posts=Post.objects.filter(status=0)
