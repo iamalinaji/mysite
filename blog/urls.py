b6fdc1b8cc0c2407831c1d8ddde58418382fb6c2
iamalinaji <anm33918@gmail.com>
2023-05-15 18:51:48 +0330

    Handled redirect to default post ID when accessing '/blog/single/' without post ID.

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from blog.views import *
 
-app_name='blog'
+app_name = 'blog'
 urlpatterns = [
-    path('',blog_view,name='home'),
-    path('single/<int:post_id>',blog_single,name='single'),
-    path('test',test,name='test'),
+    path('', blog_view, name='home'),
+    path('single/', redirect_to_single_default, name='single_default'),
+    path('single/<int:post_id>', blog_single, name='single'),
+    path('test/', test, name='test'),
 ]
-
--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,32 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone
 from django.http import Http404
 import datetime
+from django.shortcuts import redirect
 
 
 def blog_view(request):
     current_time = timezone.now()
     posts = Post.objects.filter(published_date__lt=current_time)
     context = {'posts': posts}
-    return render(request,'blog/blog-home.html',context)
+    return render(request, 'blog/blog-home.html', context)
 
-def blog_single(request,post_id=1):
-    post = get_object_or_404(Post, pk=post_id)
-    context={'post':post}
-    post.counted_views+=1
+
+def redirect_to_single_default(request):
+    return redirect('blog:single', post_id=1)
+
+
+def blog_single(request, post_id):
+    current_time = timezone.now()
+    post = get_object_or_404(Post, pk=post_id, published_date__lt=current_time)
+    context = {'post': post}
+    post.counted_views += 1
     post.save()
-    return render(request,'blog/blog-single.html',context)
+    return render(request, 'blog/blog-single.html', context)
+
 
 def test(request):
-    posts=Post.objects.filter(status=0)
+    posts = Post.objects.filter(status=0)
     context = {'posts': posts}
-    return render(request,'blog/test.html',context)
-    
-
-# Create your views here.
+    return render(request, 'blog/test.html', context)
