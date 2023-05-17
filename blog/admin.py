d4a99a01f5f8be98d17eaf462cc4fd838d489807
iamalinaji <anm33918@gmail.com>
2023-05-17 22:09:18 +0330

    Added author field to 'Post' model.

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from blog.models import Post
 
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    date_hierarchy="created_date"
-    list_display = ('title', 'counted_views', 'status','published_date')
+    date_hierarchy = "created_date"
+    list_display = ('title', 'author', 'counted_views',
+                    'status', 'published_date')
     empty_value_display = '-empty'
-    #fields
+    # fields
     list_filter = ('status', )
-    search_fields=['title','content']
-
-    
+    search_fields = ['title', 'content']
--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
 class Post(models.Model):
-    title=models.CharField(max_length=255)
-    content=models.TextField()
-    counted_views=models.IntegerField(default=0)
-    status=models.BooleanField(default=False)
-    published_date=models.DateTimeField(null=True)
-    created_date=models.DateTimeField(auto_now_add=True)
-    updated_date=models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ('-created_date',)
