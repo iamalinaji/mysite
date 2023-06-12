d44d3b8f469485650705ec2de5918173b91f3f74
iamalinaji <anm33918@gmail.com>
2023-06-12 20:04:56 +0330

    Imporoved 'Categories' and implemented 'Popular Posts' elements in blog home.

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from blog.models import Post
+from blog.models import Post, Category
 
 register = template.Library()
 
@@ -7,3 +7,18 @@ register = template.Library()
 @register.simple_tag(name='totalPosts')
 def totalPosts():
     return Post.objects.all.filter(status=1).count()
+
+
+@register.inclusion_tag('blog/blog-popular-post.html')
+def blogPopularPost(arg=3):
+    return {'posts': Post.objects.all().filter(status=1).order_by('published_date')[:arg]}
+
+
+@register.inclusion_tag('blog/blog-category.html')
+def blogCategory():
+    posts = Post.objects.all().filter(status=1)
+    categories = Category.objects.all()
+    categoryDict = {}
+    for name in categories:
+        categoryDict[name] = posts.filter(category=name).count()
+    return {'categories': categoryDict}
