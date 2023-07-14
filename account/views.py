b6a2bc01938fa15a26d6aad3f1730bb5c51f007d
iamalinaji <anm33918@gmail.com>
2023-07-14 22:26:14 +0330

    Added user authentication to project.

--- account/views.py
+++ account/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
 
 
 def login_view(request):
-    return render(request, 'account/login.html')
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = AuthenticationForm(request=request, data=request.POST)
+            if form.is_valid():
+                username = form.cleaned_data["username"]
+                password = form.cleaned_data["password"]
+                user = authenticate(
+                    request, username=username, password=password)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
+        return render(request, 'account/login.html')
+
+    else:
+        return redirect('/')
 
 
 def logout_view(request):
